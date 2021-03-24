4c53907f4f89d78e73442585a372804ae6580727
unknown <jialiang0601@gmail.com>
2021-03-23 20:03:34 -0400

    trying to fix heroku server bug

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 3001
-const utils = require("./utils")
-const path = require("path")
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const utils = require("./utils");
+const path = require("path");
 
 //Secrets
 // if (process.env.NODE_ENV !== "production") {
-require("dotenv").config()
+require("dotenv").config();
 // }
 
-console.clear()
+console.clear();
 
-app.use(express.static(path.join(__dirname, "build")))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //SQL Connect
-const mySqlConnection = require("./SQL-config")
+const mySqlConnection = require("./SQL-config");
 mySqlConnection.connect((err) => {
    if (err) {
       console.log(
          "Database not connected! : " + JSON.stringify(err, undefined, 2)
-      )
-   } else console.log("Database Connected!")
-})
+      );
+   } else console.log("Database Connected!");
+});
 
-const authRoutes = require("./routes/auth.js")
-app.use(authRoutes)
-const commentReviewRoutes = require("./routes/comment_review")
-app.use(commentReviewRoutes)
+const authRoutes = require("./routes/auth.js");
+app.use(authRoutes);
+const commentReviewRoutes = require("./routes/comment_review");
+app.use(commentReviewRoutes);
 
 // app.get("/api/test", (req, res) => {
 //    const test = req.headers
@@ -37,49 +37,49 @@ app.use(commentReviewRoutes)
 // })
 
 app.get("/api/search/prophets", (req, res) => {
-   utils.refreshToken(req)
+   utils.refreshToken(req);
 
    //Default query values
-   let orderBySort = "ORDER BY score DESC"
-   let whereScoreAbove = "WHERE score > -1"
-   let whereKeyWord = ""
-   let whereProphetID = ""
+   let orderBySort = "ORDER BY score DESC";
+   let whereScoreAbove = "WHERE score > -1";
+   let whereKeyWord = "";
+   let whereProphetID = "";
 
-   const { sort, scoreAbove, keyWord, prophetID } = req.query
+   const { sort, scoreAbove, keyWord, prophetID } = req.query;
 
    if (sort) {
-      orderBySort = `ORDER BY score ${sort}`
+      orderBySort = `ORDER BY score ${sort}`;
    }
    if (scoreAbove) {
-      whereScoreAbove = `WHERE score > ${scoreAbove}`
+      whereScoreAbove = `WHERE score > ${scoreAbove}`;
    }
    if (keyWord) {
-      whereKeyWord = `AND (name LIKE "%${keyWord}%" OR description LIKE "%${keyWord}%")`
+      whereKeyWord = `AND (name LIKE "%${keyWord}%" OR description LIKE "%${keyWord}%")`;
    }
    if (prophetID) {
-      whereProphetID = `AND prophet_id = ${prophetID}`
+      whereProphetID = `AND prophet_id = ${prophetID}`;
    }
 
-   const query = `SELECT * FROM prophets ${whereScoreAbove} ${whereKeyWord} ${whereProphetID} ${orderBySort}`
+   const query = `SELECT * FROM prophets ${whereScoreAbove} ${whereKeyWord} ${whereProphetID} ${orderBySort}`;
    utils
       .sqlPromise(query)
       .then((result) => {
-         res.json({ status: 0, result })
+         res.json({ status: 0, result });
       })
       .catch((err) => {
-         res.json({ status: 1, err })
-         console.log(err)
-      })
-})
+         console.log(err);
+         res.json({ status: 1, err });
+      });
+});
 
 app.get("/api/search/predictions", (req, res) => {
-   utils.refreshToken(req)
+   utils.refreshToken(req);
    //Default query values
-   let orderBySort = "ORDER BY score DESC"
-   let whereScoreAbove = "WHERE score >= -1"
-   let whereKeyWord = ""
-   let whereProphetID = ""
-   let wherePredictionID = ""
+   let orderBySort = "ORDER BY score DESC";
+   let whereScoreAbove = "WHERE score >= -1";
+   let whereKeyWord = "";
+   let whereProphetID = "";
+   let wherePredictionID = "";
 
    const {
       sort,
@@ -89,53 +89,53 @@ app.get("/api/search/predictions", (req, res) => {
       predictionID,
       page,
       numPerPage,
-   } = req.query
+   } = req.query;
 
    if (sort) {
-      orderBySort = `ORDER BY score ${sort}`
+      orderBySort = `ORDER BY score ${sort}`;
    }
    if (scoreAbove) {
-      whereScoreAbove = `WHERE score > ${scoreAbove}`
+      whereScoreAbove = `WHERE score > ${scoreAbove}`;
    }
    if (keyWord) {
-      whereKeyWord = `AND (title LIKE "%${keyWord}%" OR description LIKE "%${keyWord}%")`
+      whereKeyWord = `AND (title LIKE "%${keyWord}%" OR description LIKE "%${keyWord}%")`;
    }
    if (prophetID) {
-      whereProphetID = `AND prophet_id = ${prophetID}`
+      whereProphetID = `AND prophet_id = ${prophetID}`;
    }
 
    if (predictionID) {
-      wherePredictionID = `AND prediction_id = ${predictionID}`
+      wherePredictionID = `AND prediction_id = ${predictionID}`;
    }
 
-   const query = `SELECT * FROM predictions ${whereScoreAbove} ${whereKeyWord} ${whereProphetID} ${wherePredictionID} ${orderBySort}`
+   const query = `SELECT * FROM predictions ${whereScoreAbove} ${whereKeyWord} ${whereProphetID} ${wherePredictionID} ${orderBySort}`;
    let showLoadMoreButton = true,
-      result = []
+      result = [];
    utils
       .sqlPromise(query)
       .then((response) => {
          if (page) {
-            const startIndex = (page - 1) * numPerPage
-            const endIndex = page * numPerPage
-            result = response.slice(startIndex, endIndex)
+            const startIndex = (page - 1) * numPerPage;
+            const endIndex = page * numPerPage;
+            result = response.slice(startIndex, endIndex);
             if (endIndex >= response.length) {
-               showLoadMoreButton = false
+               showLoadMoreButton = false;
             }
-         } else result = response
+         } else result = response;
          // console.log(response.length)
          // console.log(result.length)
-         res.json({ status: 0, result: result, showLoadMoreButton })
+         res.json({ status: 0, result: result, showLoadMoreButton });
       })
       .catch((err) => {
-         res.json({ status: 1, err })
-         console.log(err)
-      })
-})
+         console.log(err);
+         res.json({ status: 1, err });
+      });
+});
 
 app.get("/*", (req, res) => {
-   res.sendFile(path.join(__dirname, "build", "index.html"))
-})
+   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => {
-   console.log(`Server Started at http://localhost:${port}`)
-})
+   console.log(`Server Started at http://localhost:${port}`);
+});
